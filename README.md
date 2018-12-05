50c0028687d67b2fa2a38b7e4abac8b61daf7b56
Timothée Malard <t.malard@valdun.net>
2018-12-05 14:25:08 +0100

    ADD eslint config

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# karma-eslint-config
+# eslint-config-karma
+
 The Karma EsLint rules
+
+## Installation
+```
+npm install eslint-config-karma eslint eslint-plugin-import eslint-config-airbnb --save-dev
+```
+
+## Set up a .eslintrc in your project
+```
+{
+  "extends": "karma",
+  "rules" : {
+  }
+}
+
+```
+
+## Add it to your project's package.json
+```
+{
+  "scripts": {
+    "lint": "eslint .",
+  }
+}
+```
+
+## Run the linter
+```
+npm run lint
+```
---
+++ index.js
@@ -0,0 +1,60 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
+  plugins: [
+    'flowtype',
+    'flow-vars'
+  ],
+  rules: {
+    'flowtype/require-parameter-type': 1,
+    'flowtype/space-after-type-colon': [
+      1,
+      'always'
+    ],
+    'flowtype/space-before-type-colon': [
+      1,
+      'never'
+    ],
+    'flowtype/type-id-match': [
+      1,
+      '^([A-Z][a-z0-9]+)+Type$'
+    ],
+    'flow-vars/define-flow-type': 1,
+    'flow-vars/use-flow-type': 1,
+    'new-cap': 0,
+    'no-param-reassign': [
+      1,
+      {
+        'props': false
+      }
+    ],
+    'no-unused-vars': [
+      1,
+      {
+        'args': 'none'
+      }
+    ],
+    'consistent-return': 0
+  },
+  globals: {
+    describe: true,
+    it: true,
+    before: true,
+    after: true,
+    beforeEach: true,
+    afterEach: true
+  },
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'global'
+  },
+  parser: 'babel-eslint',
+};
