a9c88a172f5212fe59513f920b3eb5aff0fdaf10
Romain Bourjot <romain.bourjot@qare.io>
2019-10-05 22:34:44 +0200

    Prettify index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     'airbnb',
     'plugin:import/errors',
-    'plugin:import/warnings'
+    'plugin:import/warnings',
   ],
   plugins: [
     'flowtype',
@@ -12,38 +12,19 @@ module.exports = {
   rules: {
     'no-only-tests/no-only-tests': 'error',
     'flowtype/require-parameter-type': 1,
-    'flowtype/space-after-type-colon': [
-      1,
-      'always'
-    ],
-    'flowtype/space-before-type-colon': [
-      1,
-      'never'
-    ],
-    'flowtype/type-id-match': [
-      1,
-      '^([A-Z][a-z0-9]+)+Type$'
-    ],
+    'flowtype/space-after-type-colon': [ 1, 'always' ],
+    'flowtype/space-before-type-colon': [ 1, 'never' ],
+    'flowtype/type-id-match': [ 1, '^([A-Z][a-z0-9]+)+Type$' ],
     'flow-vars/define-flow-type': 1,
     'flow-vars/use-flow-type': 1,
     'guard-for-in': 0,
     'import/prefer-default-export': 0,
     'new-cap': 0,
     'no-await-in-loop': 0,
-    'no-param-reassign': [
-      1,
-      {
-        'props': false
-      }
-    ],
-    'no-unused-vars': [
-      1,
-      {
-        'args': 'none'
-      }
-    ],
+    'no-param-reassign': [ 1, { 'props': false } ],
+    'no-unused-vars': [ 1, { 'args': 'none' } ],
     'no-restricted-syntax': 0,
-    'consistent-return': 0
+    'consistent-return': 0,
   },
   globals: {
     describe: true,
@@ -51,7 +32,7 @@ module.exports = {
     before: true,
     after: true,
     beforeEach: true,
-    afterEach: true
+    afterEach: true,
   },
   env: {
     es6: true,
@@ -59,7 +40,7 @@ module.exports = {
     mocha: true
   },
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   parser: 'babel-eslint',
 };
