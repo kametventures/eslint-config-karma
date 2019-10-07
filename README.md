735319f1ae6f07f649332d63816216639d0f5219
romainbourjot-qare <53749657+romainbourjot-qare@users.noreply.github.com>
2019-10-07 10:53:06 +0200

    Fix/technical (#7)
    * Fix author and description
    
    * Remove local eslint
    
    * Remove unused parserOption ecmaVersion
    
    * Prettify index.js
    
    * Order rules from A to Z
    
    * Replace deprecated flow-vars with flowtyped
    
    * Fix dependencies
    
    * Improve README
    
    * Bump version to 1.2.2

--- README.md
+++ README.md
@@ -2,22 +2,53 @@
 
 The Qare EsLint rules
 
-## Installation
+## Contributing
+
+1. Checkout and pull master
+
+`git checkout master && git pull origin master`
+
+2. Create and checkout a branch
+
+`git branch -b your-branch`
+
+3. Do your changes
+4. **Bump the package version in package.json** ([following semver](https://semver.org/))
+5. Push your changes
+
+`git push origin your-branch`
+
+6. Open a Pull Request towards master on github
+7. On approval: **SQASH AND MERGE** so that one commit is one version
+8. Create a new tag on master corresponding to the package version (ie. `v1.2.8`)
+
+## Installation in a project
+
+### Install this package
 ```
-npm install eslint-config-qare eslint eslint-plugin-import eslint-config-airbnb --save-dev
+yarn add -D eslint-config-qare@git+https://github.com/qare/eslint-config.git
 ```
 
-## Set up a .eslintrc in your project
+### Install all peer dependencies
 ```
-{
-  "extends": "qare",
-  "rules" : {
-  }
-}
+yarn add -D eslint@^6.1.0
+yarn add -D eslint-plugin-flowtype@^4.3.0
+yarn add -D eslint-plugin-import@^2.18.2
+yarn add -D eslint-plugin-no-only-tests@^2.3.1
+yarn add -D eslint-plugin-jsx-a11y@^6.2.3
+yarn add -D eslint-plugin-react@^7.14.3
+yarn add -D eslint-plugin-react-hooks@^1.7.0
+```
+
+### Set up a .eslintrc.js file at root level
+```
+module.exports = {
+  extends: [ 'qare' ],
+};
 
 ```
 
-## Add it to your project's package.json
+### Add it to your project's package.json
 ```
 {
   "scripts": {
@@ -26,7 +57,7 @@ npm install eslint-config-qare eslint eslint-plugin-import eslint-config-airbnb 
 }
 ```
 
-## Run the linter
+### Run the linter
 ```
-npm run lint
+yarn lint
 ```
--- index.js
+++ index.js
@@ -2,48 +2,28 @@ module.exports = {
   extends: [
     'airbnb',
     'plugin:import/errors',
-    'plugin:import/warnings'
+    'plugin:import/warnings',
   ],
   plugins: [
     'flowtype',
-    'flow-vars',
     'no-only-tests',
   ],
   rules: {
-    'no-only-tests/no-only-tests': 'error',
+    'consistent-return': 0,
+    'flowtype/define-flow-type': 1,
     'flowtype/require-parameter-type': 1,
-    'flowtype/space-after-type-colon': [
-      1,
-      'always'
-    ],
-    'flowtype/space-before-type-colon': [
-      1,
-      'never'
-    ],
-    'flowtype/type-id-match': [
-      1,
-      '^([A-Z][a-z0-9]+)+Type$'
-    ],
-    'flow-vars/define-flow-type': 1,
-    'flow-vars/use-flow-type': 1,
+    'flowtype/space-after-type-colon': [ 1, 'always' ],
+    'flowtype/space-before-type-colon': [ 1, 'never' ],
+    'flowtype/type-id-match': [ 1, '^([A-Z][a-z0-9]+)+Type$' ],
+    'flowtype/use-flow-type': 1,
     'guard-for-in': 0,
     'import/prefer-default-export': 0,
     'new-cap': 0,
     'no-await-in-loop': 0,
-    'no-param-reassign': [
-      1,
-      {
-        'props': false
-      }
-    ],
-    'no-unused-vars': [
-      1,
-      {
-        'args': 'none'
-      }
-    ],
+    'no-only-tests/no-only-tests': 'error',
+    'no-param-reassign': [ 1, { 'props': false } ],
     'no-restricted-syntax': 0,
-    'consistent-return': 0
+    'no-unused-vars': [ 1, { 'args': 'none' } ],
   },
   globals: {
     describe: true,
@@ -51,7 +31,7 @@ module.exports = {
     before: true,
     after: true,
     beforeEach: true,
-    afterEach: true
+    afterEach: true,
   },
   env: {
     es6: true,
@@ -59,8 +39,7 @@ module.exports = {
     mocha: true
   },
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   parser: 'babel-eslint',
 };
